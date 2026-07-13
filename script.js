51899186c1618fba313312ccfb7cf7b757da6e34
Jeslyn Yong <lien@Jeslyns-MacBook-Air.local>
2026-07-13 21:21:05 +0800

    Add video player

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const player = document.getElementById("player");
 
-player.src = "";
+// 先播放固定测试影片
+player.src = "media/test.mp4";
+
+// 自动播放
+player.autoplay = true;
+
+// 手机播放必须静音才能自动播放
+player.muted = true;
+
+// 循环播放
+player.loop = true;
+
+// 播放失败时忽略错误（例如浏览器限制）
+player.play().catch(() => {});
